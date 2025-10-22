432357b00149f65d9cbe3fb105b27241ec6e5203
Oleg Kudelkin <tryhard.work@mail.ru>
2025-10-22 16:27:23 +0300

    feat: fix

--- src/features/ImageSRC/ui/ImageSRC.tsx
+++ src/features/ImageSRC/ui/ImageSRC.tsx
@@ -2,7 +2,9 @@ import Image, { type ImageProps } from 'next/image';
 import type { FC } from 'react';
 
 export const ImageSRC: FC<ImageProps> = (props) => {
-	const base = `${process.env.NEXT_PUBLIC_PROD ? process.env.NEXT_PUBLIC_API_URL_PROD_STORAGE : process.env.NEXT_PUBLIC_API_URL_DEV_STORAGE}`;
+	// const base = `${process.env.NEXT_PUBLIC_PROD ? process.env.NEXT_PUBLIC_API_URL_PROD_STORAGE : process.env.NEXT_PUBLIC_API_URL_DEV_STORAGE}`;
+	const base =
+		'https://kitchen-helper-server-production.up.railway.app/storage/';
 	const { sizes, ...rest } = props;
 	const isFill = (rest as ImageProps).fill === true;
 	const computedSizes = isFill ? (sizes ?? '100vw') : sizes;
