0332cc177947ab9b945737581612d8e0beea20a0
Oleg Kudelkin <tryhard.work@mail.ru>
2025-10-25 18:19:01 +0300

    feat: fix images bugs

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
-	isProd: false,
+	isProd: true,
 	APP_BACKEND_URL: 'http://127.0.0.1:8000',
 	APP_BACKEND_URL_PROD:
 		'https://kitchen-helper-server-production.up.railway.app',
