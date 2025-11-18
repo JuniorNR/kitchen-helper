fc358fb1d329452dfbea629c690c5f0d5bafe8a7
Oleg Kudelkin <tryhard.work@mail.ru>
2025-11-18 03:59:04 +0300

    feat: fix

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,6 +1,6 @@
 export const apiConfig = {
-	isProd: true,
 	APP_BACKEND_URL: 'http://127.0.0.1:8000',
+	isProd: true,
 	APP_BACKEND_URL_PROD:
 		'https://kitchen-helper-server-production.up.railway.app',
 };
